cfb2e26fe914470ab58336b66b855926cf014fc9
Ikit24 <szszattila2@gmail.com>
2025-06-28 14:18:39 +0300

    code running functionality finished

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,4 +1,4 @@
-import os
+import os, subprocess
 
 def run_python_file(working_directory, file_path):
     full_path = os.path.join(working_directory, file_path)
@@ -8,10 +8,35 @@ def run_python_file(working_directory, file_path):
     if not absolute_path.startswith(work_dir_abspath):
             return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
 
-    if not os.path.exists(os.path.dirname(full_path)):
+    if not os.path.exists(full_path):
         return f'Error: File "{file_path}" not found.'
 
     if not absolute_path.endswith(".py"):
         return f'Error: "{file_path}" is not a Python file.'
 
-    subprocess.run(run_python.py, capture_output=True, timeout=30)
+    try:
+        filename = os.path.basename(file_path)
+        result = subprocess.run(['python3', filename],
+                                capture_output=True,
+                                timeout=30,
+                                cwd=working_directory)
+
+        stdout_str = result.stdout.decode('utf-8')
+        stderr_str = result.stderr.decode('utf-8')
+
+        output = ""
+        if stdout_str:
+            output += f"STDOUT:{stdout_str}"
+        if stderr_str:
+            output += f"STDERR:{stderr_str}"
+
+        if result.returncode != 0:
+            output += f"Process exited with code {result.returncode}"
+
+        if not output:
+            return "No output produced."
+
+        return output
+
+    except Exception as e:
+            return f"Error: executing Python file: {e}"
