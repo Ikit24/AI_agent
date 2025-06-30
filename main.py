6b4c64dab35f6190074024e316b1c88dec495b48
Ikit24 <szszattila2@gmail.com>
2025-06-30 20:37:03 +0300

    started working on func declaration

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import sys
 from dotenv import load_dotenv
-from google import genai
+import google.generativeai as genai
 from google.genai import types
-
+#from google.generativeai.types import FunctionDeclaration, Schema, Type
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -19,13 +20,33 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
-model_name = "models/gemini-1.5-pro"
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
+model_name = "models/gemini-1.5-flash"
+
+config = types.GenerateContentConfig(
+        tools=[available_functions],
+        system_instruction=system_prompt
+)
 
 response = client.models.generate_content(
     model=model_name,
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=config
 )
 
 if "--verbose" in sys.argv:
@@ -33,4 +54,8 @@ if "--verbose" in sys.argv:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-print(response.text)
+if response.function_calls:
+    for i in response.function_calls:
+        print(f"Calling function: {i.name}({i.args})")
+else:
+    print(f"{response.text}")
